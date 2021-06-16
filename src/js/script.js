f2b094b1f696c5a182fade3e0ad4aaa6b0d1ce1c
Leva <leva22062007@gmaul.com>
2021-06-16 11:37:34 +0300

    rows

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carusel__inner').slick({
+        speed: 770,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg></button>',
+        nextArrow: '<button type="button" class="slick-next"><img="../icons/right.svg"></button>'
+
+        });
+  });
