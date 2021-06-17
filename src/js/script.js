72a78f4133d66d63cdb39d4f976790e81ffd5804
Leva <leva22062007@gmaul.com>
2021-06-17 19:58:53 +0300

    js

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('.carusel__inner').slick({
         speed: 770,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg></button>',
-        nextArrow: '<button type="button" class="slick-next"><img="../icons/right.svg"></button>'
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
 
         });
   });
