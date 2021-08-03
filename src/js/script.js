e80c249aba4736092c21ac45839f228e7e6a2bd9
Leva <leva22062007@gmaul.com>
2021-08-03 20:12:02 +0300

    gg

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
                 breakpoint: 992,
                 settings: {
                     dots: true,
-                    arrows: true
+                    arrows: false
                 }
             }
         ]
